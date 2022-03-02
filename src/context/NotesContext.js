89c68bf7260bd22ff9d505284bb86d5177ed52db
itspatys <patryk.pierzchaa@gmail.com>
2022-03-02 20:56:31 +0100

    Added notes to session storage.

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -1,10 +1,13 @@
 import { createContext, useState } from "react"
+import useSesstionStorage from "../hooks/useSessionStorage"
 
 const NotesContext = createContext()
 
 const NotesProvider = ({ children }) => {
 
-    const [notes, setNotes] = useState([])
+    // const [notes, setNotes] = useState([])
+    const [notes, setNotes] = useSesstionStorage("notes", [])
+    console.log(notes)
 
     return (
         <NotesContext.Provider value={{notes, setNotes}}>
