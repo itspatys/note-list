9fc68c5f5422a1ddc3aef994cc599c790b0405e3
itspatys <patryk.pierzchaa@gmail.com>
2022-03-02 12:30:39 +0100

    Added wrapper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header"
+
+import Header from "./components/Header/Header"
+import Wrapper from "./components/Wrapper/Wrapper"
+import NoteForm from "./components/NoteForm/NoteForm"
 
 const App = () => {
 	return (
 		<div className="App">
 			<Header />
-			<Routes>
-				<Route path="/" element={<></>}></Route>
-			</Routes>
+			<Wrapper>
+				<Routes>
+					<Route path="/" element={
+						<>
+							<NoteForm />
+						</>
+					}></Route>
+				</Routes>
+			</Wrapper>
 		</div>
 );
 }
