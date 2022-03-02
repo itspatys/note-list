3305176bcf4da4c85a3c25a7c3c9ea6a2e2fbb0c
itspatys <patryk.pierzchaa@gmail.com>
2022-03-02 12:30:15 +0100

    Added header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <header className="Header">
+            <div className="Header__text">Notes App</div>
+        </header>
+    )
+}
+
+export default Header
